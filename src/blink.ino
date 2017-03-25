c02b98f6c27d0c67667373010567bc8f877f6341
Bas van Leeuwen <leeuwen@gmail.com>
2017-03-25 16:24:30 +0100

    theremin

--- src/blink.ino
+++ src/blink.ino
@@ -1,10 +1,52 @@
+#include "math.h"
+
+int rd = 0;
+
+const int ledPin = 3;
+const int speakPin = 2;
+const int trigPin = 13;
+const int echoPin = 12;
+const int lightPin = A0;
+
+
 void setup() {
-  pinMode(13, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(lightPin, INPUT);
+
+    Serial.begin(9600);
+    Serial.println("Echo v2");
 }
 
+
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(1000);           
-  digitalWrite(13, LOW); 
-  delay(1000);           
-}
+        long duration, distance, light, length;
+
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+
+        duration = pulseIn(echoPin, HIGH);
+        distance = (duration/2) / 29.1;
+
+        light = analogRead(lightPin);
+
+        Serial.print("Light: ");
+        Serial.println(light);
+
+        length = map(light, 950, 1050, 0, 100);
+
+        if (distance >= 200 || distance <= 10){
+            Serial.println("Out of range");
+            digitalWrite(speakPin, LOW);
+        }
+        else {
+            tone(speakPin, map(distance, 10, 200, 50, 4000), length);
+            Serial.print(distance);
+            Serial.println(" cm");
+        }
+        delay(map(light, 950, 1000, 0, length));
+    }
